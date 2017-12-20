0d05c2cc769cf330702a4b295156827af36382b2
Matt Drake <mcdtwb@gmail.com>
2017-12-19 20:54:21 -0500

    fixing tabs

--- leap/leap.h
+++ leap/leap.h
@@ -2,20 +2,15 @@
 #define LEAP_H
 
 namespace leap {
-
-	bool is_leap_year(int year) {
-		if (year % 4 == 0) {
-			if (year % 100 == 0) {
-				return (year % 400 == 0);
-			}
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
+    bool is_leap_year(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return (year % 400 == 0);
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
-
-#endif
-
